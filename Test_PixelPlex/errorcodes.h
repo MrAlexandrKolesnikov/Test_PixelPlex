e0df5d3fa4c8facdbb7f72e964730a99e4f2b2df
Alexandr Kolesnikov <mr.alexandr.kolesnikov@gmail.com>
2017-10-12 00:12:01 +0300

    Test_PixelPlex/ fix errorcodes.h comments and delete OK code

--- Test_PixelPlex/errorcodes.h
+++ Test_PixelPlex/errorcodes.h
@@ -2,17 +2,16 @@
  * @file   errorcodes.h
  * @Author Kolesnikov Alexandr(mr.alexandr.kolesnikov@gmail.com)
  * @date   October, 2017
- * @brief  Header file provides function command codes
+ * @brief  Header file provides function error codes
  **/
 
 #ifndef ERRORCODES_H
 #define ERRORCODES_H
 
-/** Перечисление кодов команд, передаваемых для обработки в УВК                */
+/** Enumeration of error codes                                                  */
 typedef enum
 {
-    OK                          = 0,/**<No Errors                               */
-    BAD_CMD_LINE_PARAM             ,/**<Bad input parameters                    */
+    BAD_CMD_LINE_PARAM           =1,/**<Bad input parameters                    */
     OPEN_FILE_ERROR                 /**<Error open file                         */
 }ERROR_CODE;
 
